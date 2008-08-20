c9bab4e439912adadccd0ec6b635f912cd4d3086
yellowstar6 <yellowstar6@c5c7942f-0371-78ac-df32-25810005d0b4>
2008-08-20 18:40:07 +0000

    The DLStation plugin can now assemble DS Download Station captures. No known bugs with this are known right now.

--- trunk/wmb_asm/WMB/Output/MingW/WMB_private.h
+++ trunk/wmb_asm/WMB/Output/MingW/WMB_private.h
@@ -7,11 +7,11 @@
 #define WMB_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"2.0.1.145"
+#define VER_STRING	"2.0.1.154"
 #define VER_MAJOR	2
 #define VER_MINOR	0
 #define VER_RELEASE	1
-#define VER_BUILD	145
+#define VER_BUILD	154
 #define COMPANY_NAME	"yellowstar"
 #define FILE_VERSION	"2.0b r2"
 #define FILE_DESCRIPTION	"Plugin for assembling WMB captures"
