9c2dd06b0542b05f53758e4e1d5e41e0e336d9ef
yellowstar6 <yellowstar6@c5c7942f-0371-78ac-df32-25810005d0b4>
2010-04-14 20:12:38 +0000

    Commited libyellhttp, a simple http library written in C. HTTPS support is currently broken, CyaSSL is used. Doesn't support sending If-Modified-Since headers, eventually this will be supported. Then wmb-asm apps using wget will be switched to using libyellhttp, and apps using curl will be switched to libyellhttp as well once HTTPS works properly. libyellhttp doesn't send any user agent.

---
+++ libyellhttp/yellhttptest/source/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <yellhttp.h>
+
+int main(int argc, char **argv)
+{
+	int retval;	
+	printf("yellhttptest\n");
+	if(argc!=2)return -2;
+	YellHttp_Ctx *ctx = YellHttp_InitCtx();
+	if(ctx==NULL)
+	{
+		printf("initctx alloc fail\n");
+		return -1;
+	}
+	printf("Executing DoRequest...\n");
+	retval = YellHttp_DoRequest(ctx, argv[1]);
+	printf("retval = %d\n", retval);	
+	YellHttp_FreeCtx(ctx);
+	return 0;
+}
+
