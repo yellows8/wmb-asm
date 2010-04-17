11e6aa00f3118ec3d455eb4fa78daa4d7d459002
yellowstar6 <yellowstar6@c5c7942f-0371-78ac-df32-25810005d0b4>
2010-04-17 16:45:32 +0000

    Added libyellhttp support for NDS and Wii. The Wii version doesn't work currently. Added fields to allow setting/sending custom user agent and request types. Added function to set custom header handler callbacks. Added error defines and YellHttp_GetErrorStr function. Added proper code to keep receiving content until the length of the content defined by the Content-Length header is received.

--- libyellhttp/include/yellhttp.h
+++ libyellhttp/include/yellhttp.h
@@ -2,19 +2,21 @@
 
 #include <time.h>
 
-#define YELLHTTP_SRVFLAG_RESUME 1<<0
+#define YELLHTTP_SRVFLAG_RESUME 1<<0//The server supports resuming if this flag is set. libyellhttp doesn't support resuming though.
 #define YELLHTTP_SRVFLAG_DISABLEREDIR 1<<1//optional flag to disable automatically sending a request for redirection.
 #define YELLHTTP_SRVFLAG_NOCACHE 1<<2//optional flag to disable caching: don't send the If-Modified-Since header, always download the file regardless of modification timestamp.
 #define YELLHTTP_SRVFLAG_USRFLAGS YELLHTTP_SRVFLAG_DISABLEREDIR | YELLHTTP_SRVFLAG_NOCACHE //Flags that the user can set, used to determine which flags to not clear.
 
+#define YELLHTTP_EINVAL -1
+#define YELLHTTP_ESSLDISABLED -2
+#define YELLHTTP_EDNSRESOLV -3
+#define YELLHTTP_ESOCK -4
+#define YELLHTTP_ECONN -5
+#define YELLHTTP_EFILE -6
+#define YELLHTTP_LASTERROR -6
+
 typedef struct sYellHttp_Ctx
 {
-	char url[256];
-	char uri[256];
-	char hostname[256];
-	char portstr[8];
-	char filename[256];
-	char redirecturl[256];
 	unsigned short port;
 	int SSL;
 	int sock_client;
@@ -22,15 +24,27 @@ typedef struct sYellHttp_Ctx
 	unsigned char *recvbuf;
 	unsigned int http_status;
 	unsigned int server_flags;
+	unsigned int content_length;
 	time_t server_date;//Current timestamp from server Date header.
 	time_t lastmodified;//From Last-Modified server header.
+
+	char url[256];
+	char uri[256];
+	char hostname[256];
+	char portstr[8];
+	char filename[256];
+	char redirecturl[256];
+	char useragent[256];//YellHttp_InitCtx sets this to the default user agent, which is "libyellhttp v%s" where %s is the version number.
+	char request_type[8];//If not set, default is GET.(This isn't modified when it is not set.)
 } YellHttp_Ctx;
 
-typedef void (*YellHttp_HeaderCb)(char *hdr, char *hdrfield, char *hdrval, YellHttp_Ctx *ctx);
+typedef void (*YellHttp_HeaderCb)(char *hdr, char *hdrfield, char *hdrval, YellHttp_Ctx *ctx, void* usrarg);
 
 YellHttp_Ctx *YellHttp_InitCtx();
 void YellHttp_FreeCtx(YellHttp_Ctx *ctx);
-int YellHttp_DoRequest(YellHttp_Ctx *ctx, char *url);
+int YellHttp_ExecRequest(YellHttp_Ctx *ctx, char *url);
+int YellHttp_SetHeaderCb(YellHttp_HeaderCb cb, char *header);//Sets a header handler callback. These callbacks are global, they are not specific to YellHttp_Ctx structs. If cb is NULL, the callback handler is removed. This function can override default libyellhttp hdr handlers, see yellhttp.c source for currently implementated handlers.
+void YellHttp_GetErrorStr(int error, char *errstr, int errstrlen);
 
 #endif
 
--- libyellhttp/yellhttptest/source/main.c
+++ libyellhttp/yellhttptest/source/main.c
@@ -3,21 +3,215 @@
 #include <string.h>
 #include <yellhttp.h>
 
+#ifdef ARM9
+#include <nds.h>
+#include <fat.h>
+#include <dswifi9.h>
+
+void init_nds();
+void OnKeyPressed(int key);
+#endif
+
+#ifdef HW_RVL
+#include <ogcsys.h>
+#include <gccore.h>
+#include <network.h>
+#include <debug.h>
+#include <errno.h>
+#include <wiiuse/wpad.h>
+#include <fat.h>
+
+void init_wii();
+
+static void *xfb = NULL;
+static GXRModeObj *rmode = NULL;
+
+char localip[16];
+char gateway[16];
+char netmask[16];
+#endif
+
+void console_pause();
+
 int main(int argc, char **argv)
 {
 	int retval;	
+	char *url;
+	char errstr[256];
+
+	#ifdef ARM9
+	init_nds();
+	Keyboard *kbd = keyboardDemoInit();
+	kbd->OnKeyPressed = OnKeyPressed;	
+	url = (char*)malloc(256);
+	memset(url, 0, 256);
+	scanf("%s", url);
+	argc = 2;
+	#endif
+	
+	#ifdef HW_RVL
+	init_wii();
+	
+	url = (char*)malloc(256);
+	memset(url, 0, 256);
+	strncpy(url, "http://192.168.1.33/", 256);
+	argc = 2;
+	#endif
+	
+	#ifdef WIN32
 	printf("yellhttptest\n");
+	if(argc==2)url = argv[1];	
+	#endif
+
+	#ifdef LINUX
+	printf("yellhttptest\n");
+	if(argc==2)url = argv[1];
+	#endif
+
 	if(argc!=2)return -2;
 	YellHttp_Ctx *ctx = YellHttp_InitCtx();
 	if(ctx==NULL)
 	{
 		printf("initctx alloc fail\n");
+		#ifdef ARM9		
+		free(url);
+		console_pause();
+		#endif
+		#ifdef HW_RVL
+		free(url);
+		console_pause();
+		#endif
 		return -1;
 	}
-	printf("Executing DoRequest...\n");
-	retval = YellHttp_DoRequest(ctx, argv[1]);
-	printf("retval = %d\n", retval);	
+	memset(errstr, 0, 256);
+	printf("Executing ExecRequest...(URL: %s)\n", url);
+	retval = YellHttp_ExecRequest(ctx, url);
+	YellHttp_GetErrorStr(retval, errstr, 256);
+	printf("retval = %d str: %s", retval, errstr);
 	YellHttp_FreeCtx(ctx);
+
+	#ifdef ARM9
+	free(url);
+	console_pause();
+	#endif
+	#ifdef HW_RVL
+	free(url);
+	console_pause();
+	#endif
 	return 0;
 }
+
+#ifdef ARM9
+void console_pause()
+{
+	while(1)
+	{
+		scanKeys();
+		if(keysDown() & KEY_A)exit(0);
+		swiWaitForVBlank();
+	}
+}
+
+void init_nds()
+{
+	consoleDemoInit();
+	printf("yellhttptest\n");
+	printf("Initializing FAT...\n");
+	if(!fatInitDefault())
+	{
+		printf("FAT init failed.\n");
+		console_pause();
+	}
+	printf("Connecting via WFC data ...\n");
+
+	if(!Wifi_InitDefault(WFC_CONNECT))
+	{
+		printf("Failed to connect!\nPress A to exit.\n");
+		console_pause();
+	}
+	printf("Connected.\n");
+}
+
+void OnKeyPressed(int key) {
+   if(key > 0)
+      iprintf("%c", key);
+}
+#endif
+
+#ifdef HW_RVL
+void console_pause()
+{
+	while(1)
+	{
+		VIDEO_WaitVSync();
+		WPAD_ScanPads();
+		
+		if (WPAD_ButtonsDown(0) & WPAD_BUTTON_HOME)exit(0);
+	}
+}
+
+void init_wii()
+{
+	// Initialise the video system
+	VIDEO_Init();
+	
+	// This function initialises the attached controllers
+	WPAD_Init();
+	
+	// Obtain the preferred video mode from the system
+	// This will correspond to the settings in the Wii menu
+	rmode = VIDEO_GetPreferredMode(NULL);
+
+	// Allocate memory for the display in the uncached region
+	xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+	
+	// Initialise the console, required for printf
+	console_init(xfb,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
+	
+	// Set up the video registers with the chosen mode
+	VIDEO_Configure(rmode);
+	
+	// Tell the video hardware where our display memory is
+	VIDEO_SetNextFramebuffer(xfb);
+	
+	// Make the display visible
+	VIDEO_SetBlack(FALSE);
+
+	// Flush the video register changes to the hardware
+	VIDEO_Flush();
+
+	// Wait for Video setup to complete
+	VIDEO_WaitVSync();
+	if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
+
+
+	// The console understands VT terminal escape codes
+	// This positions the cursor on row 2, column 0
+	// we can use variables for this with format codes too
+	// e.g. printf ("\x1b[%d;%dH", row, column );
+	printf("\x1b[2;0H");
+
+	printf("yellhttptest\n");
+	/*printf("Initializing FAT...\n");
+	if(!fatInitDefault())
+	{
+		printf("FAT init failed.\n");
+		console_pause();
+	}*/
+	printf("Configuring network ...\n");
+	memset(localip, 0, 16);
+	memset(netmask, 0, 16);
+	memset(gateway, 0, 16);
+	s32 ret = if_config (localip, netmask, gateway, true);
+	if(ret<0)
+	{
+		printf("Network config failed: %d\n", ret);
+		console_pause();
+	}
+	else
+	{
+		printf("Network config done.\n");
+	}
+}
+#endif
 
